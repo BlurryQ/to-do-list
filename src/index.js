2979ed1482621be40ea5652125879fb0fe59a1c8
BlurryQ <jazz.barlow@outlook.com>
2024-08-07 20:46:59 +0100

    Added function to display only lists from specific projects

--- src/index.js
+++ src/index.js
@@ -1,101 +1,118 @@
-
-import style from "./style.css"
-import DATA from "./modules/data.js"
-import INTERFACE from "./modules/interface.js"
-import GET_DATE from "./modules/get-date.js"
+import style from "./style.css";
+import DATA from "./modules/data.js";
+import INTERFACE from "./modules/interface.js";
+import GET_DATE from "./modules/get-date.js";
 
 /* ---------- ---------- ---------- ---------- ---------- */
 const controller = () => {
+  const Interface = INTERFACE();
+  const Data = DATA();
+  const getDate = GET_DATE();
+  const TO_DO_DOM = document.getElementById("to-do");
+  const CURRENT_PROJECTS_DOM = document.getElementById("current-projects");
+  const ALL_PROJECTS_BUTTON = document.getElementById("all-projects");
 
-    const Interface = INTERFACE()
-    const Data = DATA()
-    const getDate = GET_DATE()
-    const TO_DO = document.getElementById("to-do")
-    const CURRENT_PROJECTS = document.getElementById("current-projects")
+  ALL_PROJECTS_BUTTON.addEventListener("click", () => {
+    updatePegboard(true);
+  });
 
-    const projectsToDisplay = () => {
-        const toDoLists = Data.get()
-        const allProjects = []
-        for(const list in toDoLists) {
-            allProjects.push(toDoLists[list].project)
-        }
-        return allProjects
+  const createProjectsSidebar = (removeChildContent) => {
+    if (removeChildContent) Interface.removeChildContent(CURRENT_PROJECTS_DOM);
+    const projects = projectsToDisplay();
+    Interface.displayProjects(projects, CURRENT_PROJECTS_DOM);
+
+    const projectButtons = document.querySelectorAll('[name="project"]');
+    projectButtons.forEach((project) => {
+      project.addEventListener("click", () => {
+        Interface.removeChildContent(TO_DO_DOM);
+
+        const projectLists = getProjectLists(project.textContent);
+        Interface.displayLists(projectLists, TO_DO_DOM);
+      });
+    });
+  };
+
+  const projectsToDisplay = () => {
+    const toDoLists = Data.get();
+    const allProjects = [];
+    for (const list in toDoLists) {
+      const projectName = toDoLists[list].project;
+      if (!allProjects.includes(projectName)) allProjects.push(projectName);
+      continue;
     }
+    return allProjects;
+  };
 
-    //create side bar
-    const createProjectsSidebar = (removeChildContent) => {
-        if(removeChildContent) Interface.removeChildContent(CURRENT_PROJECTS)
-        const projects = projectsToDisplay()
-        Interface.displayProjects(projects, CURRENT_PROJECTS)
-    }
-        //pass down list of projects
+  const getProjectLists = (projectName) => {
+    return Data.get().filter((list) => list.project === projectName);
+  };
 
-    //listeners for side bar
+  const addToDoList = () => {
+    const newForm = document.getElementById("new-form");
+    Data.add(Interface.getToDoListData(newForm), getDate.formatted());
+    Interface.clearFormValues(newForm);
+    newForm.style.cssText = "display: none;";
+  };
 
-        //if pressed only show specific projects
+  const updateToDoList = (formID) => {
+    const updatedForm = document.getElementById(formID);
+    const formIndex = getIndexFromID(formID);
+    Data.update(Interface.getToDoListData(updatedForm), formIndex);
+  };
 
-    //logic function for sidebar
+  const removeToDoList = (formID) => {
+    const index = getIndexFromID(formID);
+    Data.remove(index);
+    updatePegboard(true);
+  };
 
-        //if textContent === toDoList.project > Interface.displayProjectLists(projectToDisplay)
+  const startFormListener = () => {
+    toggleNewFormListener();
+    const FORMS = document.querySelectorAll("form");
+    FORMS.forEach((form) => {
+      form["save"].addEventListener("click", () => {
+        form.id === "new-form" ? addToDoList() : updateToDoList(form.id);
+        createProjectsSidebar(true);
+        updatePegboard(true);
+      });
+      form["remove"].addEventListener("click", () => {
+        removeToDoList(form.id);
+        createProjectsSidebar(true);
+      });
+    });
+  };
 
-    const addToDoList = () => {
-        const newForm = document.getElementById("new-form")
-        Data.add(Interface.getToDoListData(newForm), getDate.formatted())
-        Interface.clearFormValues(newForm)
-        newForm.style.cssText = "display: none;" 
-    }
+  const toggleNewFormListener = () => {
+    TO_DO_DOM.appendChild(Interface.toDoList());
+    const NEW_FORM = document.getElementById("new-form");
+    NEW_FORM.style.cssText = "display: none;";
+    const TOGGLE_NEW_FORM = document.getElementById("toggle-new-form");
+    TOGGLE_NEW_FORM.addEventListener(
+      "click",
+      () =>
+        (NEW_FORM.style.cssText =
+          NEW_FORM.style.cssText === "display: flex;"
+            ? "display: none;"
+            : "display: flex;")
+    );
+  };
 
-    const updateToDoList = (formID) => {
-        const updatedForm = document.getElementById(formID)
-        const formIndex = getIndexFromID(formID)
-        Data.update(Interface.getToDoListData(updatedForm), formIndex)
-    }
+  const getIndexFromID = (formID) => {
+    //formID = `index-${array index}`
+    const splitID = formID.split("-");
+    return splitID[1];
+  };
 
-    const removeToDoList = (formID) => {
-        const index = getIndexFromID(formID)
-        Data.remove(index)
-        updatePegboard(true)
-    }
-    
-    const startFormListener = () => {
-        toggleNewFormListener()
-        const FORMS = document.querySelectorAll("form")
-        FORMS.forEach(form => {
-            form["save"].addEventListener("click", () => {
-                form.id === "new-form" ? addToDoList() : updateToDoList(form.id)
-                createProjectsSidebar(true)
-                updatePegboard(true)
-            })
-            form["remove"].addEventListener("click", () => {
-                removeToDoList(form.id)
-                createProjectsSidebar(true)
-            })
-        })
-    }
-    
-    const toggleNewFormListener = () => {
-        TO_DO.appendChild(Interface.toDoList()) 
-        const NEW_FORM = document.getElementById("new-form")
-        NEW_FORM.style.cssText = "display: none;"
-        const TOGGLE_NEW_FORM = document.getElementById("toggle-new-form")
-        TOGGLE_NEW_FORM.addEventListener("click", () => NEW_FORM.style.cssText = NEW_FORM.style.cssText === "display: flex;" ? "display: none;" : "display: flex;"
-        )
-    }
+  const updatePegboard = (removeChildContent) => {
+    if (removeChildContent) Interface.removeChildContent(TO_DO_DOM);
+    Interface.displayLists(Data.get(), TO_DO_DOM);
+    startFormListener();
+  };
 
-    const getIndexFromID = (formID) => {
-        //formID = `index-${array index}`
-        const splitID = formID.split("-")  
-        return splitID[1]   
-    }
+  createProjectsSidebar(false);
+  updatePegboard(false);
+};
 
-    const updatePegboard = (removeChildContent) => {
-        if(removeChildContent) Interface.removeChildContent(TO_DO)
-        Interface.displayLists(Data.get(), TO_DO);
-        startFormListener()
-    }
+controller();
 
-    createProjectsSidebar(false)
-    updatePegboard(false)
-}
-
-controller()
+//add empty form error
--- src/modules/interface.js
+++ src/modules/interface.js
@@ -1,159 +1,164 @@
 /* import newForm from "./functions/newForm.js" */
 
-export default function Interface () {
+export default function Interface() {
+  const displayProjects = (projects, CURRENT_PROJECTS) => {
+    for (const project of projects) {
+      const projectButton = document.createElement("button");
+      projectButton.textContent = project;
+      projectButton.name = "project";
+      CURRENT_PROJECTS.appendChild(projectButton);
+    }
+    return CURRENT_PROJECTS;
+  };
 
-    const displayProjects = (projects, CURRENT_PROJECTS) => {
-        for(const project of projects) {
-            const projectButton = document.createElement("button")
-            projectButton.textContent = project
-            CURRENT_PROJECTS.appendChild(projectButton)
-        }
-        return CURRENT_PROJECTS
+  const displayProjectLists = (projectToDisplay) => {};
+
+  const toDoList = (toDoList, listIndex) => {
+    const form = document.createElement("form");
+    const projectLabel = document.createElement("label");
+    const titleLabel = document.createElement("label");
+    const toDoLabel = document.createElement("label");
+    const dueDateLabel = document.createElement("label");
+    const projectInput = document.createElement("input");
+    const titleInput = document.createElement("input");
+    const toDoInput = document.createElement("input");
+    const dueDateInput = document.createElement("input");
+    const projectBR = document.createElement("br");
+    const titleBR = document.createElement("br");
+    const toDoBR = document.createElement("br");
+    const dueDateBR = document.createElement("br");
+    const save = document.createElement("button");
+    const remove = document.createElement("button");
+
+    projectLabel.for = "project";
+    projectLabel.textContent = "Project Name:";
+    projectInput.type = "text";
+    projectInput.name = "project";
+    projectInput.placeholder = "Enter project name";
+
+    titleLabel.for = "title";
+    titleLabel.textContent = "Title:";
+    titleInput.type = "text";
+    titleInput.name = "title";
+    titleInput.placeholder = "Enter title";
+
+    toDoLabel.for = "to-do";
+    toDoLabel.textContent = "To Do:";
+    toDoInput.type = "text";
+    toDoInput.name = "to-do";
+    toDoInput.placeholder = "Enter note here";
+
+    dueDateLabel.for = "dueDate";
+    dueDateLabel.textContent = "Date due:";
+    dueDateInput.type = "date";
+    dueDateInput.name = "dueDate";
+    dueDateInput.placeholder = "Enter due date";
+
+    save.textContent = "Save";
+    remove.textContent = "Delete";
+
+    save.type = "button";
+    remove.type = "button";
+
+    form.setAttribute("id", "new-form");
+    projectInput.setAttribute("name", "project");
+    titleInput.setAttribute("name", "title");
+    toDoInput.setAttribute("name", "to-do");
+    dueDateInput.setAttribute("name", "due-date");
+    save.setAttribute("name", "save");
+    remove.setAttribute("name", "remove");
+
+    form.appendChild(projectLabel);
+    form.appendChild(projectInput);
+    form.appendChild(projectBR);
+    form.appendChild(titleLabel);
+    form.appendChild(titleInput);
+    form.appendChild(titleBR);
+    form.appendChild(toDoLabel);
+    form.appendChild(toDoInput);
+    form.appendChild(toDoBR);
+    form.appendChild(dueDateLabel);
+    form.appendChild(dueDateInput);
+    form.appendChild(dueDateBR);
+
+    if (toDoList) {
+      projectInput.value = toDoList.project;
+      titleInput.value = toDoList.title;
+      toDoInput.value = toDoList["to-do"];
+      dueDateInput.value = toDoList["due-date"];
+
+      const createdDateLabel = document.createElement("label");
+      const createdDateSpan = document.createElement("span");
+      const createdDateBR = document.createElement("br");
+
+      createdDateLabel.textContent = "Created Date:";
+      createdDateLabel.for = "created-date";
+      createdDateSpan.textContent = toDoList.created;
+
+      createdDateSpan.classList.add("createdDate");
+
+      form.setAttribute("id", "index-" + listIndex);
+
+      form.appendChild(createdDateLabel);
+      form.appendChild(createdDateSpan);
+      form.appendChild(createdDateBR);
     }
 
-    const displayProjectLists = (projectToDisplay) => {
+    form.appendChild(save);
+    form.appendChild(remove);
 
+    return form;
+  };
+
+  const _addListToggle = () => {
+    const toggle = document.createElement("button");
+    toggle.setAttribute("id", "toggle-new-form");
+    toggle.textContent = "+";
+    return toggle;
+  };
+
+  const getToDoListData = (HTMLForm) => {
+    return {
+      project: HTMLForm["project"].value,
+      title: HTMLForm["title"].value,
+      "to-do": HTMLForm["to-do"].value,
+      "due-date": HTMLForm["due-date"].value,
+    };
+  };
+
+  const clearFormValues = (HTMLForm) => {
+    HTMLForm["project"].value = "";
+    HTMLForm["title"].value = "";
+    HTMLForm["to-do"].value = "";
+    HTMLForm["due-date"].value = "";
+  };
+
+  const removeChildContent = (...parents) => {
+    for (const parent of parents) {
+      while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+      }
     }
+  };
 
-    const toDoList = (toDoList, listIndex) => {
-        const form = document.createElement("form")
-        const projectLabel = document.createElement("label")
-        const titleLabel = document.createElement("label")
-        const toDoLabel = document.createElement("label")
-        const dueDateLabel = document.createElement("label")
-        const projectInput = document.createElement("input")
-        const titleInput = document.createElement("input")
-        const toDoInput = document.createElement("input")
-        const dueDateInput = document.createElement("input")
-        const projectBR = document.createElement("br")
-        const titleBR = document.createElement("br")
-        const toDoBR = document.createElement("br")
-        const dueDateBR = document.createElement("br")
-        const save = document.createElement("button")
-        const remove = document.createElement("button")
-    
-        projectLabel.for = "project"
-        projectLabel.textContent = "Project Name:"
-        projectInput.type = "text"
-        projectInput.name = "project"
-        projectInput.placeholder = "Enter project name"
-    
-        titleLabel.for = "title"
-        titleLabel.textContent = "Title:"
-        titleInput.type = "text"
-        titleInput.name = "title"
-        titleInput.placeholder = "Enter title"
-    
-        toDoLabel.for = "to-do"
-        toDoLabel.textContent = "To Do:"
-        toDoInput.type = "text"
-        toDoInput.name = "to-do"
-        toDoInput.placeholder = "Enter note here"
-    
-        dueDateLabel.for = "dueDate"
-        dueDateLabel.textContent = "Date due:"
-        dueDateInput.type = "date"
-        dueDateInput.name = "dueDate"
-        dueDateInput.placeholder = "Enter due date"
-    
-        save.textContent = "Save"
-        remove.textContent = "Delete"
-
-        save.type = "button"
-        remove.type = "button"
-    
-        form.setAttribute("id", "new-form")
-        projectInput.setAttribute("name", "project")
-        titleInput.setAttribute("name", "title")
-        toDoInput.setAttribute("name", "to-do")
-        dueDateInput.setAttribute("name", "due-date")
-        save.setAttribute("name", "save")
-        remove.setAttribute("name", "remove")
-    
-        form.appendChild(projectLabel)
-        form.appendChild(projectInput)
-        form.appendChild(projectBR)
-        form.appendChild(titleLabel)
-        form.appendChild(titleInput)
-        form.appendChild(titleBR)
-        form.appendChild(toDoLabel)
-        form.appendChild(toDoInput)
-        form.appendChild(toDoBR)
-        form.appendChild(dueDateLabel)
-        form.appendChild(dueDateInput)
-        form.appendChild(dueDateBR)
-
-        if(toDoList) {
-            projectInput.value = toDoList.project
-            titleInput.value = toDoList.title
-            toDoInput.value = toDoList["to-do"]
-            dueDateInput.value = toDoList["due-date"]
-
-            const createdDateLabel = document.createElement("label")
-            const createdDateSpan = document.createElement("span")
-            const createdDateBR = document.createElement("br")
-
-            createdDateLabel.textContent = "Created Date:"
-            createdDateLabel.for = "created-date"
-            createdDateSpan.textContent = toDoList.created
-
-            createdDateSpan.classList.add("createdDate")
-
-            form.setAttribute("id", "index-" + listIndex)
-            
-            form.appendChild(createdDateLabel)
-            form.appendChild(createdDateSpan)
-            form.appendChild(createdDateBR)
-        }
-
-        form.appendChild(save)
-        form.appendChild(remove)
-        
-        return form
+  const displayLists = (toDoLists, location) => {
+    if (toDoLists) {
+      let listIndex = 0;
+      for (const _list of toDoLists) {
+        const list = toDoList(_list, listIndex);
+        location.appendChild(list);
+        listIndex += 1;
+      }
     }
+    location.appendChild(_addListToggle());
+  };
 
-    const _addListToggle = () => {
-        const toggle = document.createElement("button")
-        toggle.setAttribute("id", "toggle-new-form")
-        toggle.textContent = "+"
-        return toggle
-    }
-    
-    const getToDoListData = (HTMLForm) => {
-        return {
-            project: HTMLForm["project"].value,
-            title: HTMLForm["title"].value,
-            "to-do": HTMLForm["to-do"].value,
-            "due-date": HTMLForm["due-date"].value
-        }
-    }
-    
-        const clearFormValues = (HTMLForm) => {
-            HTMLForm["project"].value = ""
-            HTMLForm["title"].value = ""
-            HTMLForm["to-do"].value = ""
-            HTMLForm["due-date"].value = ""
-        }
-    
-    const removeChildContent = (...parents) => {
-        for(const parent of parents) {
-            while(parent.firstChild) {
-                parent.removeChild(parent.firstChild)
-            }
-        }
-    }
-
-    const displayLists = (toDoLists, location) => {
-        if(toDoLists) {
-        let listIndex = 0
-        for(const _list of toDoLists) {
-
-            const list = toDoList(_list, listIndex)
-            location.appendChild(list)
-            listIndex += 1
-        }}
-        location.appendChild(_addListToggle())
-    }
-    
-    return { displayProjects, toDoList, getToDoListData, clearFormValues, removeChildContent, displayLists }
+  return {
+    displayProjects,
+    toDoList,
+    getToDoListData,
+    clearFormValues,
+    removeChildContent,
+    displayLists,
+  };
 }
